23bb58a43fb0bbc83503ed42a4e79d2d99e35175
vampur <yoitsvampur@gmail.com>
2022-09-04 21:25:59 +0200

    move watermark a little bit

--- src/features/visuals.cpp
+++ src/features/visuals.cpp
@@ -39,6 +39,7 @@ namespace Visuals {
     void watermark() {
         ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.15f, 0.15f, 0.16f, 1.00f));
         ImGui::SetNextWindowSizeConstraints(ImVec2(5, 5), ImVec2(600, 25));
+        ImGui::SetNextWindowPos(ImVec2(5,5));
         ImGui::Begin("watermark", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | (Menu::menuOpen ? ImGuiWindowFlags_None : ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoMouseInputs));
         ImGui::Text("eclipse.wtf %s | %.1fFPS", Protection::username, ImGui::GetIO().Framerate);
         ImGui::End();
